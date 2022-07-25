9ade9e9c589fad87d31ef9de36c490a5c4164a4e
Niv Caspi <niv0caspi@gmail.com>
2022-07-25 14:08:07 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,30 @@
+def create_youtube_video(title, description):
+	youtube_video = {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}}
+	return youtube_video
+
+def like(youtube_video):
+	if "likes" in youtube_video:
+		youtube_video["likes"] += 1
+
+def dislike(youtube_video):
+	if "dislikes" in youtube_video:
+		youtube_video["dislikes"] += 1
+
+def add_comment(youtubevideo, username, comment_text):
+	youtubevideo[username] = comment_text
+	return youtubevideo
+
+
+def main():
+	new_youtube_dict = create_youtube_video("Summer Y2", "Meet's Summer")
+	like(new_youtube_dict)
+	dislike(new_youtube_dict)
+	add_comment(new_youtube_dict, "Tal", "I am excited!")
+	print("Title: ", new_youtube_dict['title'])
+	print("Description: ", new_youtube_dict['description'])
+	print(new_youtube_dict['likes'], " people liked this :)")
+	print(new_youtube_dict['dislikes'], " people disliked this :(")
+	print("Comments: \n==========")
+	print(new_youtube_dict['comments'])
+if __name__ == '__main__':
+	main()
